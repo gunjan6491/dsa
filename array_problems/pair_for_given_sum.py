c4a4f4180c670a969f51ebf71cfdb2b91d23e4ce
Gunjan Aswani <gunjanaswani6491@gmail.com>
2026-02-20 10:47:24 +0530

    solved pair for elements to make the target sum problem

--- array_problems/pair_for_given_sum.py
+++ array_problems/pair_for_given_sum.py
@@ -1,4 +1,6 @@
-arr=[2, 7, 11, 15,4,5]
+arr=[2,7,11,15,4,5]
 sum=9
-r=[]
-for i in 
+for i in range(len(arr)):
+    for j in range(i+1,len(arr)):
+        if arr[i]+arr[j]==sum:
+            print((arr[i],arr[j]))
--- array_problems/reversee.py
+++
@@ -1,6 +0,0 @@
-arr=[1,2,3,4,5]
-rev=[]
-for i in arr[::-1]:
-    rev.append(i)
-
-print(rev)
